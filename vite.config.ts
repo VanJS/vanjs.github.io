b6aea95cb3228db9c6212b2c63fc48f65875131e
bibianasouza <souzabibiana@hotmail.com>
2025-06-26 09:48:04 -0700

    fix: update base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vanjs-website/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
